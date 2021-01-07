c3a55ea6061614309c6f1a963b5ce3063d87ecc3
hd962q <77110448+hd962q@users.noreply.github.com>
2021-01-07 16:54:06 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,9 @@
-resource "aws" {
-  region = "us-east-2"
-  }
 resource "aws_vpc" "vpc2" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
+  placement {
+    availability_zone = "us-east-2b"
+  }
 
   tags = {
     Name = "main"
