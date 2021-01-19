a0e832c4c0dbbb9e1985226390eb9c14466975d0
hd962q <77110448+hd962q@users.noreply.github.com>
2021-01-19 13:36:57 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -4,7 +4,6 @@ provider "aws" {
 resource "aws_security_group" "sg" {
   name        = "sg"
   description = "Allow traffic"
-  vpc_id      = aws_vpc.vpc.id
 
   ingress {
     description = "AT&T from VPC"
